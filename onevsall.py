57806da528019af7747d2c700f26da8a219404ce
Jatin Karthik Tripathy <jatinkarthikt@gmail.com>
2023-01-22 06:12:30 +0100

    expert model done

--- onevsall.py
+++ onevsall.py
@@ -1,82 +1,10 @@
-import os
-import torch
-import numpy as np
 import pandas as pd
-import pytorch_lightning as pl
-from torch.utils.data import Dataset, DataLoader
-from transformers import AutoModel, AutoTokenizer
 from sklearn.model_selection import train_test_split
-
-class ExpertDataset(Dataset):
-
-  def __init__(self, df, tokenizer, labels, max_token_len: int = 128, sample = 5000):
-    self.data = df
-    self.tokenizer = tokenizer
-    self.labels = labels
-    self.max_token_len = max_token_len
-    self.sample = sample
-
-  def __len__(self):
-    return len(self.data)
-
-  def __getitem__(self, index):
-    item = self.data.iloc[index]
-    comment = str(item.comment)
-    labels = torch.FloatTensor(item[self.labels])
-    tokens = self.tokenizer.encode_plus(comment,
-                                        add_special_tokens=True,
-                                        return_tensors='pt',
-                                        truncation=True,
-                                        padding='max_length',
-                                        max_length=self.max_token_len,
-                                        return_attention_mask = True)
-    return {'input_ids': tokens.input_ids.flatten(), 'attention_mask': tokens.attention_mask.flatten(), 'labels': labels}
-
-
-class ExpertDataModule(pl.LightningDataModule):
-
-  def __init__(self, train_df, val_df, labels, batch_size: int = 16, max_token_length: int = 128,  model_name='roberta-base'):
-    super().__init__()
-    self.train_df = train_df
-    self.val_df = val_df
-    self.labels = labels
-    self.batch_size = batch_size
-    self.max_token_length = max_token_length
-    self.model_name = model_name
-    self.tokenizer = AutoTokenizer.from_pretrained(model_name)
-
-  def setup(self, stage = None):
-    if stage in (None, "fit"):
-      self.train_dataset = ExpertDataset(self.train_df, labels=self.labels, tokenizer=self.tokenizer)
-      self.val_dataset = ExpertDataset(self.val_df, labels=self.labels, tokenizer=self.tokenizer, sample=None)
-    if stage == 'predict':
-      self.val_dataset = ExpertDataset(self.val_df, labels=self.labels, tokenizer=self.tokenizer, sample=None)
-
-  def train_dataloader(self):
-    return DataLoader(self.train_dataset, batch_size = self.batch_size, num_workers=4, shuffle=True)
-
-  def val_dataloader(self):
-    return DataLoader(self.val_dataset, batch_size = self.batch_size, num_workers=4, shuffle=False)
-
-  def predict_dataloader(self):
-    return DataLoader(self.val_dataset, batch_size = self.batch_size, num_workers=4, shuffle=False)
+import pytorch_lightning as pl
+from onevsall_expert import ExpertDataModule, ExpertClassifier
 
 if __name__ == "__main__":
-    #device = 0 if torch.cuda.is_available() else -1
-    #model = AutoModel.from_pretrained("microsoft/deberta-v2-xxlarge")
-    #tokenizer = AutoTokenizer.from_pretrained("microsoft/deberta-v2-xxlarge")
-    #pipe = pipeline(
-    #        'feature-extraction',
-    #        model = model,
-    #        tokenizer = tokenizer,
-    #        device = device,
-    #)
-    #res = pipe("Test sentence")
-    #print(res)
-
-
     df = pd.read_csv('project/Explainable-Sexism/data/train_all_tasks.csv')
-
     label_map = {
             'none':0,
             '1. threats, plans to harm and incitement' : 1,
@@ -94,6 +22,22 @@ if __name__ == "__main__":
         binary_df = df.copy(deep=True)
         binary_df['label'].replace(new_map, inplace=True)
         
-        X_train, X_test, Y_train, Y_test = train_test_split(df['text'], df['label'], test_size=0.2)
+        df_train, df_test = train_test_split(df, test_size=0.2)
+        dm = ExpertDataModule(df_train, df_test)
+        dm.setup()
+        expert_config = {
+            'model_name': "microsoft/deberta-large",
+            'n_labels': 2,
+            'batch_size': 128,
+            'lr': 1.5e-6,
+            'warmup': 0.2, 
+            'train_size': len(dm.train_dataloader()),
+            'weight_decay': 0.001,
+            'n_epochs': 100
+        }
+        expert_model = ExpertClassifier(expert_config)
+
+        trainer = pl.Trainer(max_epochs=expert_config['n_epochs'], gpus=1)
+        trainer.fit(expert_model, dm)
 
 
---
+++ onevsall_expert.py
@@ -0,0 +1,118 @@
+import math
+import torch
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+import pytorch_lightning as pl
+from torch.utils.data import Dataset, DataLoader
+from transformers import AutoModel, AutoTokenizer
+from transformers import AdamW, get_cosine_schedule_with_warmup
+
+class ExpertDataset(Dataset):
+
+    def __init__(self, df, tokenizer, max_token_len: int = 128, sample = 5000):
+        self.data = df
+        self.tokenizer = tokenizer
+        self.max_token_len = max_token_len
+        self.sample = sample
+        self._prepare_data()
+
+    def _prepare_data(self):
+        self.data['class'] = np.where(self.data['label'] == 1, 1, 0)
+        self.data['notClass'] = np.where(self.data['label'] == 0, 1, 0)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        item = self.data.iloc[index]
+        text = str(item.text)
+        labels = torch.FloatTensor([item['class'], item['notClass']])
+        tokens = self.tokenizer.encode_plus(text,
+                                            add_special_tokens=True,
+                                            return_tensors='pt',
+                                            truncation=True,
+                                            padding='max_length',
+                                            max_length=self.max_token_len,
+                                            return_attention_mask = True
+                                            )
+        return {'input_ids': tokens.input_ids.flatten(), 'attention_mask': tokens.attention_mask.flatten(), 'labels': labels}
+
+
+class ExpertDataModule(pl.LightningDataModule):
+
+    def __init__(self, train_df, val_df, batch_size: int = 16, max_token_length: int = 128,  model_name='roberta-base'):
+        super().__init__()
+        self.train_df = train_df
+        self.val_df = val_df
+        self.batch_size = batch_size
+        self.max_token_length = max_token_length
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    def setup(self, stage = None):
+        if stage in (None, "fit"):
+            self.train_dataset = ExpertDataset(self.train_df, tokenizer=self.tokenizer)
+            self.val_dataset = ExpertDataset(self.val_df, tokenizer=self.tokenizer, sample=None)
+        if stage == 'predict':
+            self.val_dataset = ExpertDataset(self.val_df, tokenizer=self.tokenizer, sample=None)
+
+    def train_dataloader(self):
+        return DataLoader(self.train_dataset, batch_size = self.batch_size, num_workers=4, shuffle=True)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_dataset, batch_size = self.batch_size, num_workers=4, shuffle=False)
+
+    def predict_dataloader(self):
+        return DataLoader(self.val_dataset, batch_size = self.batch_size, num_workers=4, shuffle=False)
+
+
+class ExpertClassifier(pl.LightningModule):
+
+    def __init__(self, config: dict):
+        super().__init__()
+        self.config = config
+        self.pretrained_model = AutoModel.from_pretrained(config['model_name'], return_dict = True)
+        self.hidden = torch.nn.Linear(self.pretrained_model.config.hidden_size, self.pretrained_model.config.hidden_size)
+        self.classifier = torch.nn.Linear(self.pretrained_model.config.hidden_size, self.config['n_labels'])
+        torch.nn.init.xavier_uniform_(self.classifier.weight)
+        self.loss_func = nn.BCEWithLogitsLoss(reduction='mean')
+        self.dropout = nn.Dropout()
+        
+    def forward(self, input_ids, attention_mask, labels=None):
+        # roberta layer
+        output = self.pretrained_model(input_ids=input_ids, attention_mask=attention_mask)
+        pooled_output = torch.mean(output.last_hidden_state, 1)
+        # final logits
+        pooled_output = self.dropout(pooled_output)
+        pooled_output = self.hidden(pooled_output)
+        pooled_output = F.relu(pooled_output)
+        pooled_output = self.dropout(pooled_output)
+        logits = self.classifier(pooled_output)
+        # calculate loss
+        loss = 0
+        if labels is not None:
+          loss = self.loss_func(logits.view(-1, self.config['n_labels']), labels.view(-1, self.config['n_labels']))
+        return loss, logits
+    
+    def training_step(self, batch, batch_index):
+        loss, outputs = self(**batch)
+        self.log("train loss ", loss, prog_bar = True, logger=True)
+        return {"loss":loss, "predictions":outputs, "labels": batch["labels"]}
+    
+    def validation_step(self, batch, batch_index):
+        loss, outputs = self(**batch)
+        self.log("validation loss ", loss, prog_bar = True, logger=True)
+        return {"val_loss": loss, "predictions":outputs, "labels": batch["labels"]}
+    
+    def predict_step(self, batch, batch_index):
+        loss, outputs = self(**batch)
+        return outputs
+    
+    def configure_optimizers(self):
+        optimizer = AdamW(self.parameters(), lr=self.config['lr'], weight_decay=self.config['weight_decay'])
+        total_steps = self.config['train_size']/self.config['batch_size']
+        warmup_steps = math.floor(total_steps * self.config['warmup'])
+        warmup_steps = math.floor(total_steps * self.config['warmup'])
+        scheduler = get_cosine_schedule_with_warmup(optimizer, warmup_steps, total_steps)
+        return [optimizer],[scheduler]
+
