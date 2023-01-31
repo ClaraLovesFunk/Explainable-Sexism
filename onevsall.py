45195d789cd2b8e36bed43b55221e1c186fd4ac1
Jatin Karthik Tripathy <jatinkarthikt@gmail.com>
2023-01-31 08:51:24 +0100

    master model done

--- onevsall.py
+++ onevsall.py
@@ -1,14 +1,75 @@
 import torch
 import pandas as pd
-from sklearn.model_selection import train_test_split
 import pytorch_lightning as pl
+from transformers import AutoModel
+from sklearn.model_selection import train_test_split
 from pytorch_lightning.callbacks import ModelCheckpoint
 from onevsall_expert import ExpertDataModule, ExpertClassifier
+from onevsall_master import MasterDataModule, MasterClassifier
+
+
+def train_master(df, model_name, experts, num_classes=5, doTrain=False, doTest=False):
+    save_dir = "project/Explainable-Sexism/onevsall_models"
+    model_filename="master"
+
+    df_train, df_test = train_test_split(df, test_size=0.2)
+    dm = MasterDataModule(df_train, df_test, model_name=model_name)
+    dm.setup()
+        
+    config = {
+            'model_name': model_name,
+            'n_labels': num_classes,
+            'experts': experts,
+            'batch_size': 8,
+            'lr': 1e-3,
+            'warmup': 0.2, 
+            'train_size': len(dm.train_dataloader()),
+            'weight_decay': 0.001,
+            'n_epochs': 15,
+        }
+    model = MasterClassifier(config)
+        
+    checkpoint_callback = ModelCheckpoint(
+                dirpath=save_dir,
+                save_top_k=1,
+                monitor="val f1",
+                filename=model_filename
+                )
+    trainer = pl.Trainer(
+                max_epochs=config['n_epochs'],
+                accelerator='gpu',
+                devices=1,
+                num_sanity_val_steps=15,
+                default_root_dir="project/Explainable-Sexism/",
+                callbacks=[checkpoint_callback],
+                )
+
+    if not doTrain:
+        ckpt = torch.load(
+                  f"{save_dir}/{model_filename}.ckpt"
+                    )
+        model.load_state_dict(ckpt['state_dict'])
+    else:
+        trainer.fit(model, dm)
+        torch.save(
+                model.state_dict(),
+                f"{save_dir}/{model_filename}.pt"
+                )
+
+    if doTest:
+        model.eval()
+        test_dataloader = dm.test_dataloader()
+        trainer.test(model, dataloaders=test_dataloader)
+
+    return model
 
 def train_experts(df, model_name, num_classes=5, doTrain=False, doTest=False):
     expert_configs = []
     expert_models = []
+    save_dir = "project/Explainable-Sexism/onevsall_models"
     for label in range(num_classes):
+        model_filename=f"unbalanced_expert_{label}_batch8"
+
         new_map = {i:0 for i in range(num_classes)}
         new_map[label] = 1
         binary_df = df.copy(deep=True)
@@ -21,23 +82,31 @@ def train_experts(df, model_name, num_classes=5, doTrain=False, doTest=False):
         expert_config = {
             'model_name': model_name,
             'n_labels': 2,
-            'batch_size': 128,
-            'lr': 1.5e-6,
+            'batch_size': 16,
+            'lr': 1e-3,
             'warmup': 0.2, 
             'train_size': len(dm.train_dataloader()),
             'weight_decay': 0.001,
-            'n_epochs': 1,
+            'n_epochs': 3,
             }
         expert_configs.append(expert_config)
         expert_model = ExpertClassifier(expert_config)
         
         checkpoint_callback = ModelCheckpoint(
-                dirpath="project/Explainable-Sexism/onevsall_models",
+                dirpath=save_dir,
                 save_top_k=1,
                 monitor="val f1",
-                filename=f"balanced_expert_{label}",
+                filename=model_filename
                 )
-        trainer = pl.Trainer(
+
+        if not doTrain:
+            ckpt = torch.load(
+                  f"{save_dir}/{model_filename}.ckpt"
+                    )
+            expert_model.load_state_dict(ckpt['state_dict'])
+            expert_models.append(expert_model)
+        else:
+            trainer = pl.Trainer(
                 max_epochs=expert_config['n_epochs'],
                 accelerator='gpu',
                 devices=1,
@@ -45,20 +114,17 @@ def train_experts(df, model_name, num_classes=5, doTrain=False, doTest=False):
                 default_root_dir="project/Explainable-Sexism/",
                 callbacks=[checkpoint_callback],
                 )
-
-        if not doTrain:
-            ckpt = torch.load(
-                  f"project/Explainable-Sexism/onevsall_models/balanced_expert_{label}.ckpt"
-                    )
-            expert_model.load_state_dict(ckpt['state_dict'])
-            expert_models.append(expert_model)
-        else:
-            expert_models.append(expert_model)
             trainer.fit(expert_model, dm)
+            torch.save(
+                expert_model.state_dict(),
+                f"{save_dir}/{model_filename}.pt"
+                )
+            expert_models.append(expert_model)
 
         if doTest:
             expert_model.eval()
-            trainer.test(expert_model, dm)
+            test_dataloader = dm.test_dataloader()
+            trainer.test(expert_model, dataloaders=test_dataloader)
 
     return expert_models, expert_configs
 
@@ -79,9 +145,9 @@ if __name__ == "__main__":
 
     model_name = "microsoft/deberta-large"
 
-    full_experts, configs = train_experts(df, model_name, doTrain=True, doTest=False)
+    full_experts, configs = train_experts(df, model_name, doTrain=False, doTest=False)
 
-    experts = [ExpertClassifier(config) for config in configs]
+    experts = [AutoModel.from_pretrained(model_name) for _ in range(len(label_map))]
 
     for i in range(len(label_map)):
         finetuned_dict = full_experts[i].state_dict()
@@ -95,6 +161,8 @@ if __name__ == "__main__":
         experts[i].load_state_dict(model_dict)
         # freeze the weights for the master model
         experts[i].eval()
+
+    master = train_master(df, model_name, experts, doTrain=True, doTest=True)
         
 
 
--- onevsall_master.py
+++ onevsall_master.py
@@ -11,27 +11,17 @@ from transformers import AutoModel, AutoTokenizer
 from transformers import get_cosine_schedule_with_warmup
 from torchmetrics.classification import MulticlassF1Score
 
-class ExpertDataset(Dataset):
+class MasterDataset(Dataset):
 
-    def __init__(self, df, tokenizer, max_token_len: int = 128, balance=False):
+    def __init__(self, df, tokenizer, max_token_len: int = 128):
         self.data = df
         self.tokenizer = tokenizer
         self.max_token_len = max_token_len
-        self.balance = balance
         self._prepare_data()
 
     def _prepare_data(self):
-        if self.balance:
-            binary_class = self.data[self.data['label']==1]
-            binary_notClass = self.data[self.data['label']==0]
-            
-            if len(binary_class) > len(binary_notClass):
-                self.data = pd.concat([binary_notClass, binary_class.sample(len(binary_notClass), random_state=0)])
-            else: 
-                self.data = pd.concat([binary_class, binary_notClass.sample(len(binary_class), random_state=0)])
-
-        self.data['class'] = np.where(self.data['label'] == 1, 1, 0)
-        self.data['notClass'] = np.where(self.data['label'] == 0, 1, 0)
+        for i in range(5):
+            self.data[f'{i}'] = np.where(self.data['label'] == i, 1, 0)
 
     def __len__(self):
         return len(self.data)
@@ -39,7 +29,7 @@ class ExpertDataset(Dataset):
     def __getitem__(self, index):
         item = self.data.iloc[index]
         text = str(item.text)
-        labels = torch.FloatTensor([item['class'], item['notClass']])
+        labels = torch.FloatTensor([item[f'{i}'] for i in range(5)])
         tokens = self.tokenizer.encode_plus(text,
                                             add_special_tokens=True,
                                             return_tensors='pt',
@@ -51,23 +41,22 @@ class ExpertDataset(Dataset):
         return {'input_ids': tokens.input_ids.flatten(), 'attention_mask': tokens.attention_mask.flatten(), 'labels': labels}
 
 
-class ExpertDataModule(pl.LightningDataModule):
+class MasterDataModule(pl.LightningDataModule):
 
-    def __init__(self, train_df, val_df, batch_size: int = 16, max_token_length: int = 128,  model_name='roberta-base', balance=False):
+    def __init__(self, train_df, val_df, batch_size: int = 16, max_token_length: int = 128,  model_name='roberta-base'):
         super().__init__()
         self.train_df = train_df
         self.val_df = val_df
         self.batch_size = batch_size
         self.max_token_length = max_token_length
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
-        self.balance = balance
 
     def setup(self, stage = None):
         if stage in (None, "fit"):
-            self.train_dataset = ExpertDataset(self.train_df, tokenizer=self.tokenizer, balance=self.balance)
-            self.val_dataset = ExpertDataset(self.val_df, tokenizer=self.tokenizer)
+            self.train_dataset = MasterDataset(self.train_df, tokenizer=self.tokenizer)
+            self.val_dataset = MasterDataset(self.val_df, tokenizer=self.tokenizer)
         if stage == 'predict':
-            self.val_dataset = ExpertDataset(self.val_df, tokenizer=self.tokenizer)
+            self.val_dataset = MasterDataset(self.val_df, tokenizer=self.tokenizer)
 
     def train_dataloader(self):
         return DataLoader(self.train_dataset, batch_size = self.batch_size, num_workers=4, shuffle=True)
@@ -82,17 +71,22 @@ class ExpertDataModule(pl.LightningDataModule):
         return DataLoader(self.val_dataset, batch_size = self.batch_size, num_workers=4, shuffle=False)
 
 
-class ExpertClassifier(pl.LightningModule):
+class MasterClassifier(pl.LightningModule):
     def __init__(self, config: dict):
         super().__init__()
-        self.save_hyperparameters()
+        # self.save_hyperparameters()
         self.config = config
-        self.pretrained_model = AutoModel.from_pretrained(config['model_name'], return_dict = True)
+        device = torch.device("cuda")
+        self.experts = self.config['experts']
+        for exp in self.experts:
+            exp.to(device)
+            exp.eval()
+
         self.hidden = torch.nn.Linear(
-                self.pretrained_model.config.hidden_size,
-                self.pretrained_model.config.hidden_size
+                self.experts[0].config.hidden_size*self.config['n_labels'],
+                512
                 )
-        self.classifier = torch.nn.Linear(self.pretrained_model.config.hidden_size, self.config['n_labels'])
+        self.classifier = torch.nn.Linear(512, self.config['n_labels'])
         self.soft = torch.nn.Softmax(dim=1)
 
         torch.nn.init.xavier_uniform_(self.classifier.weight)
@@ -100,17 +94,32 @@ class ExpertClassifier(pl.LightningModule):
 
         self.loss_func = nn.BCEWithLogitsLoss(reduction='mean')
         self.f1_func = MulticlassF1Score(num_classes=self.config['n_labels'])
+        print('init done')
         
     def forward(self, input_ids, attention_mask, labels=None):
-        output = self.pretrained_model(input_ids=input_ids, attention_mask=attention_mask)
+        output0 = self.experts[0](input_ids=input_ids, attention_mask=attention_mask)
+        pooled_output0 = torch.mean(output0.last_hidden_state, 1)
 
-        pooled_output = torch.mean(output.last_hidden_state, 1)
+        output1 = self.experts[1](input_ids=input_ids, attention_mask=attention_mask)
+        pooled_output1 = torch.mean(output1.last_hidden_state, 1)
 
-        pooled_output = self.dropout(pooled_output)
-        pooled_output = self.hidden(pooled_output)
-        pooled_output = F.relu(pooled_output)
-        pooled_output = self.dropout(pooled_output)
-        logits = self.classifier(pooled_output)
+        output2 = self.experts[2](input_ids=input_ids, attention_mask=attention_mask)
+        pooled_output2 = torch.mean(output2.last_hidden_state, 1)
+
+        output3 = self.experts[3](input_ids=input_ids, attention_mask=attention_mask)
+        pooled_output3 = torch.mean(output3.last_hidden_state, 1)
+
+        output4 = self.experts[4](input_ids=input_ids, attention_mask=attention_mask)
+        pooled_output4 = torch.mean(output4.last_hidden_state, 1)
+
+        pooled_output = torch.cat((pooled_output0, pooled_output1, pooled_output2, pooled_output3, pooled_output4), 1)
+
+        # final logits
+        output = self.dropout(pooled_output)
+        output = self.hidden(output)
+        output = F.relu(output)
+        output = self.dropout(output)
+        logits = self.classifier(output)
         logits = self.soft(logits)
 
         # calculate loss and f1
