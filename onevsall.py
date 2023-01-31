542c5d45a792ed4ca0730e9bfc4e4df1c8dc0a24
Jatin Karthik Tripathy <jatinkarthikt@gmail.com>
2023-01-31 20:50:59 +0100

    pred and save

--- onevsall.py
+++ onevsall.py
@@ -9,9 +9,9 @@ from onevsall_expert import ExpertDataModule, ExpertClassifier
 from onevsall_master import MasterDataModule, MasterClassifier
 
 
-def train_master(df, model_name, experts, num_classes=5, doTrain=False, doTest=False, doPredict=False, pred_df=None):
+def train_master(df, model_name, experts, num_classes=5, doTrain=False, doTest=False):
     save_dir = "project/Explainable-Sexism/onevsall_models"
-    model_filename="master"
+    model_filename="master-v2"
 
     df_train, df_test = train_test_split(df, test_size=0.2)
     dm = MasterDataModule(df_train, df_test, model_name=model_name)
@@ -26,37 +26,24 @@ def train_master(df, model_name, experts, num_classes=5, doTrain=False, doTest=F
             'warmup': 0.2, 
             'train_size': len(dm.train_dataloader()),
             'weight_decay': 0.001,
-            'n_epochs': 15,
+            'n_epochs': 5,
         }
     model = MasterClassifier(config)
         
-    checkpoint_callback = ModelCheckpoint(
-                dirpath=save_dir,
-                save_top_k=1,
-                monitor="val f1",
-                filename=model_filename
-                )
     trainer = pl.Trainer(
                 max_epochs=config['n_epochs'],
                 accelerator='gpu',
                 devices=1,
                 num_sanity_val_steps=15,
                 default_root_dir="project/Explainable-Sexism/",
-                callbacks=[checkpoint_callback],
                 )
 
     if not doTrain:
-        ckpt = torch.load(
-                  f"{save_dir}/{model_filename}.pt"
-                    )
+        ckpt = torch.load(f"{save_dir}/{model_filename}.pt")
         model.load_state_dict(ckpt)
-        # model.load_state_dict(ckpt['state_dict'])
     else:
         trainer.fit(model, dm)
-        torch.save(
-                model.state_dict(),
-                f"{save_dir}/{model_filename}.pt"
-                )
+        torch.save(model.state_dict(), f"{save_dir}/{model_filename}.pt")
 
     if doTest:
         model.eval()
@@ -66,12 +53,12 @@ def train_master(df, model_name, experts, num_classes=5, doTrain=False, doTest=F
     return model
 
 
-def train_experts(df, model_name, num_classes=5, doTrain=False, doTest=False):
+def train_experts(df, model_name, num_classes=5, doBalance=False, doTrain=False, doTest=False):
     expert_configs = []
     expert_models = []
     save_dir = "project/Explainable-Sexism/onevsall_models"
     for label in range(num_classes):
-        model_filename=f"unbalanced_expert_{label}_batch8"
+        model_filename=f"unbalanced_expert_{label}_b8_e5"
 
         new_map = {i:0 for i in range(num_classes)}
         new_map[label] = 1
@@ -79,49 +66,37 @@ def train_experts(df, model_name, num_classes=5, doTrain=False, doTest=False):
         binary_df['label'].replace(new_map, inplace=True)
         
         df_train, df_test = train_test_split(binary_df, test_size=0.2)
-        dm = ExpertDataModule(df_train, df_test, balance=True)
+        dm = ExpertDataModule(df_train, df_test, balance=doBalance)
         dm.setup()
         
         expert_config = {
             'model_name': model_name,
             'n_labels': 2,
-            'batch_size': 16,
+            'batch_size': 8,
             'lr': 1e-3,
             'warmup': 0.2, 
             'train_size': len(dm.train_dataloader()),
             'weight_decay': 0.001,
-            'n_epochs': 3,
+            'n_epochs': 5,
             }
         expert_configs.append(expert_config)
         expert_model = ExpertClassifier(expert_config)
         
-        checkpoint_callback = ModelCheckpoint(
-                dirpath=save_dir,
-                save_top_k=1,
-                monitor="val f1",
-                filename=model_filename
-                )
         trainer = pl.Trainer(
                 max_epochs=expert_config['n_epochs'],
                 accelerator='gpu',
                 devices=1,
                 num_sanity_val_steps=15,
                 default_root_dir="project/Explainable-Sexism/",
-                callbacks=[checkpoint_callback],
                 )
 
         if not doTrain:
-            ckpt = torch.load(
-                  f"{save_dir}/{model_filename}.ckpt"
-                    )
-            expert_model.load_state_dict(ckpt['state_dict'])
+            ckpt = torch.load(f"{save_dir}/{model_filename}.pt")
+            expert_model.load_state_dict(ckpt)
             expert_models.append(expert_model)
         else:
             trainer.fit(expert_model, dm)
-            torch.save(
-                expert_model.state_dict(),
-                f"{save_dir}/{model_filename}.pt"
-                )
+            torch.save(expert_model.state_dict(), f"{save_dir}/{model_filename}.pt")
             expert_models.append(expert_model)
 
         if doTest:
@@ -183,7 +158,7 @@ if __name__ == "__main__":
 
     model_name = "microsoft/deberta-large"
 
-    full_experts, configs = train_experts(df, model_name, doTrain=False, doTest=False)
+    full_experts, configs = train_experts(df, model_name, doTrain=True, doTest=True)
 
     experts = [AutoModel.from_pretrained(model_name) for _ in range(len(label_map))]
 
@@ -200,12 +175,23 @@ if __name__ == "__main__":
         # freeze the weights for the master model
         experts[i].eval()
 
-    master = train_master(df, model_name, experts, doTrain=False, doTest=False)
+    master = train_master(df, model_name, experts, doTrain=True, doTest=True)
+
+    #dev 
     test1 = pd.read_csv('project/Explainable-Sexism/data/dev_task_b_entries.csv')
     test1['label'] = np.zeros(len(test1))
     df = get_preds(master, model_name, df, test1)
     df.drop(['text', '0', '1', '2', '3', '4'], inplace=True, axis=1)
     print(df.head())
+    df.to_csv('project/Explainable-Sexism/data/dev_task_b_result.csv')
+
+    #test
+    test2 = pd.read_csv('project/Explainable-Sexism/data/test_task_b_entries.csv')
+    test2['label'] = np.zeros(len(test2))
+    df = get_preds(master, model_name, df, test2)
+    df.drop(['text', '0', '1', '2', '3', '4'], inplace=True, axis=1)
+    print(df.head())
+    df.to_csv('project/Explainable-Sexism/data/test_task_b_result.csv')
         
 
 
