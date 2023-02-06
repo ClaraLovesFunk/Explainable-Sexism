cc098068e2380893ed06a488ee315642c3e397ec
Clara Wicharz <clara.wicharz@gmail.com>
2023-02-06 09:40:21 +0000

    f1-score bug solved: re-indexing of gold labels

--- EDA.py
+++ EDA.py
@@ -14,18 +14,19 @@ def load_arrange_data(data_path):
   for k in labels:
     data[k] = np.where(data['label_category'] == k, 1, 0)
     
-  label_map = {                                  
-    'none': 0,
-    '1. threats, plans to harm and incitement' : 1,
-    '2. derogation': 2,
-    '3. animosity': 3,
-    '4. prejudiced discussions': 4
+  label_map = {                                      ####### REINDEXED
+    'none': 4,
+    '1. threats, plans to harm and incitement' : 0,
+    '2. derogation': 1,
+    '3. animosity': 2,
+    '4. prejudiced discussions': 3
     }
   
   data['label_category'].replace(label_map, inplace=True) 
   
   # drop instances with no sexism in train data since no sexism is not regarded as a class in the semeval task
-  data.drop(data.loc[data['label_category']==0].index, inplace=True)
+  data.drop(data.loc[data['label_category']==4].index, inplace=True) ##### CHANGED TO 4
+  
   
   return data, labels
 
